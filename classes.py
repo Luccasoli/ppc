fc210ff602c6cb1525d745efef5c7bace1227ae4
Lucas <oliveira.mesquita@aluno.uece.br>
2018-11-04 22:02:26 -0300

    up

--- classes.py
+++ classes.py
@@ -6,22 +6,24 @@ class Veiculo():
         it = Intervalo de tempo entre o próximo veiculo de mesmo tipo
     '''
 
-    def __init__(self, tt, it=0):
+    def __init__(self, tt, origem, tipo, it=0):
         self.tt = tt
         self.it = it
+        self.origem = origem
+        self.tipo = tipo
 
     def atravessar(self):
         sleep(self.tt)
         print('Atravessei')
 
 class Carro(Veiculo):
-    def __init__(self, tt, it):
-        super().__init__(tt, it)
+    def __init__(self, tt, origem, tipo, it=0):
+        super().__init__(tt, origem, tipo, it)
 
 
 class Caminhao(Veiculo):
-    def __init__(self, tt):
-        super().__init__(tt)
+    def __init__(self, tt, origem, tipo, it=0):
+        super().__init__(tt, origem, tipo, it)
 
 
 class Ponte:
@@ -30,3 +32,20 @@ class Ponte:
         self.carros_atravessaram_para_direita = 0
         self.caminhoes_atravessaram_para_direita = 0
         self.caminhoes_atravessaram_para_esquerda = 0
+
+    def atravessou(self, origem, tipo):
+        if origem == 'esquerda':
+            if tipo == 'carro':
+                self.carros_atravessaram_para_direita += 1
+            else:
+                self.caminhoes_atravessaram_para_direita += 1
+        elif origem == 'direita':
+            if tipo == 'carro':
+                self.carros_atravessaram_para_esquerda += 1
+            else:
+                self.caminhoes_atravessaram_para_esquerda += 1
+        else:
+            print('Origem incorreta!')         
+
+    def total(self):
+        return self.caminhoes_atravessaram_para_direita+self.caminhoes_atravessaram_para_esquerda+self.carros_atravessaram_para_direita+self.carros_atravessaram_para_esquerda     
--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from time import sleep
 
 N_CARROS = 10
 N_CAMINHOES = 2
+TRAVESSIA_CARRO = 1
+TRAVESSIA_CAMINHAO = 2
+# Intervalo de geração de veículos
 TA = [1, 2]
 
 
@@ -22,7 +25,7 @@ def gera_veiculo(**kwargs):
             if randint(0, 1):
                 if carros_gerados < N_CARROS/2:
                     sleep(randint(TA[0], TA[1]))
-                    carro = Carro(10, 2)
+                    carro = Carro(TRAVESSIA_CARRO, origem, 'carro', 2)
                     fila.put_nowait(carro)
                     carros_gerados += 1
                     print('Carro {} gerado na {}'.format(carros_gerados, origem.upper()))
@@ -30,7 +33,7 @@ def gera_veiculo(**kwargs):
             else:
                 if caminhoes_gerados < N_CAMINHOES/2:
                     sleep(randint(TA[0], TA[1]))
-                    caminhao = Caminhao(20)
+                    caminhao = Caminhao(TRAVESSIA_CAMINHAO, origem, 'caminhao')
                     fila.put_nowait(caminhao)
                     caminhoes_gerados += 1
                     print('Caminhão {} gerado na {}'.format(caminhoes_gerados, origem.upper()))
@@ -45,33 +48,47 @@ def atravessa(**kwargs):
     origem_na_direita = kwargs['qr']
     cond_na_esquerda = kwargs['cond_na_esquerda']
     cond_na_direita = kwargs['cond_na_direita']
+    ponte = kwargs['ponte']
 
-    while(True):
+    while(ponte.total() < (N_CAMINHOES+N_CARROS)):
         comp = randint(0, 1)
         fila = origem_na_esquerda if comp else origem_na_direita
         condition = cond_na_esquerda if comp else cond_na_direita
         with condition:
             if fila.empty():
+                print('Sem veículos!')
                 condition.wait()
             v = fila.get_nowait()
             v.atravessar()
+            ponte.atravessou(v.origem, v.tipo)
 
 
 def main():
+    # Uma condição pra cada sentido de origem
     cond_na_esquerda = Condition()
     cond_na_direita = Condition()
+    # A ponte contabiliza os veiculos que atravessaram a mesma
+    ponte = Ponte()
+    # Armazena os veiculos de cada lado de origem
     qe = Queue()
     qr = Queue()
 
     gera_na_esquerda = Thread(target=gera_veiculo, kwargs={'fila': qe, 'condition': cond_na_esquerda, 'origem': 'esquerda'})
     gera_na_direita = Thread(target=gera_veiculo, kwargs={'fila': qr, 'condition': cond_na_direita, 'origem': 'direita'})
+    travessia = Thread(target=atravessa, kwargs=
+                                            {'qe': qe,
+                                            'qr': qr,
+                                            'cond_na_esquerda': cond_na_esquerda,
+                                            'cond_na_direita': cond_na_direita,
+                                            'ponte': ponte})
 
     gera_na_direita.start()
     gera_na_esquerda.start()
+    travessia.start()
 
     gera_na_direita.join()
     gera_na_esquerda.join()
-
+    travessia.join()
 
 if __name__ == '__main__':
     main()
